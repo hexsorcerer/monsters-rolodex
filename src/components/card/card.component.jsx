2e07170338bee1a15e4a33d4f770ae7a903e8538
hexsorcerer <hexsorcerer03e9@gmail.com>
2021-09-11 16:29:35 -0400

    feat: add search box
    Added a search box at the top of the page, which will instantly filter out cards based on the user
    name.

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -3,8 +3,8 @@ import './card.styles.css'
 
 export const Card = (props) => (
     <div className='card-container'>
-        <img alt="monster" src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} />
+        <img alt="monster" src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}/>
         <h2> {props.monster.name} </h2>
-        <p>{props.monster.email}</p>
+        <p> {props.monster.email} </p>
     </div>
 )
