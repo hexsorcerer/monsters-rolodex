d121be59c8b8004f730bd81decde677b547537c5
hexsorcerer <hexsorcerer03e9@gmail.com>
2021-09-11 17:48:10 -0400

    ci: add routes for github pages
    Previously, the application would not load correctly when deployed to github pages, it just loaded a
    blank page. This had something to do with React Routes, which I do not understand at all because
    they haven't been covered in the course yet. I managed to copy/paste some code from a medium article
    to get it working. Here it is.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+ReactDOM.render((
+        <React.StrictMode>
+            <BrowserRouter basename={`/${process.env.PUBLIC_URL}`}>
+                <Switch>
+                    <Route path='/' exact component={App} />
+                </Switch>
+                <App/>
+            </BrowserRouter>
+        </React.StrictMode>),
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
